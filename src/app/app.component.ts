bb3935c0c7c84f3e00774d7ad8d7fe8b437964b4
dominic-diblasio <dominic.c.diblasio@gmail.com>
2025-02-28 15:34:00 -0800

    Week 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecast } from './weather-forecast';
+import { environment } from '../environments/environment.development';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  styleUrl: './app.component.scss',
+  imports: [
+    NavBarComponent,
+    RouterOutlet
+  ]
 })
-export class AppComponent {
-  title = 'ddiblasioProjectClient';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  title = 'domangular.client';
 }
