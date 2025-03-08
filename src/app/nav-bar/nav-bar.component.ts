eb7d70f79f2109e5ea38803ed9402658d1d3a74f
dominic-diblasio <dominic.c.diblasio@gmail.com>
2025-03-07 18:13:11 -0800

    Class March 7th (Client-side)

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIcon } from '@angular/material/icon'
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbarModule,
+    MatIcon
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
